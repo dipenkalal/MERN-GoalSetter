141f186632968729a55f6df14bc75b03bb5459d4
Dipen Kalal <kalald@uwindsor.ca>
2023-06-04 21:46:55 -0400

    get and set method

--- backend/routes/GoalRoutes.js
+++ backend/routes/GoalRoutes.js
@@ -1,11 +1,11 @@
 //common js module syntax
 const express = require('express')
 const router = express.Router()
-const {getGoals,setGoals,putGoals,deleteGoals} =require('../controllers/goalController')
+const {getGoals,setGoals,putGoals,deleteGoals, updateGoals} =require('../controllers/goalController')
 
 //router for get and post request
 router.route('/').get(getGoals).post(setGoals)
 
 //router for put request and delete request but we require an id for updating specific value or field
-router.route('/:id').get(putGoals).post(deleteGoals)
+router.route('/:id').get(updateGoals).post(deleteGoals)
 module.exports = router
