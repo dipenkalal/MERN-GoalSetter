f522498b4afb4539a9c74f2782b2a8d91222dae9
Dipen Kalal <kalald@uwindsor.ca>
2023-06-04 20:14:34 -0400

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 //server code run by 'npm run server'
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000 
 const {errorHandler} = require('./middleware/errorMiddleware')
 
 const app = express()
+
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({
